9a713d050bc7974cfbb2a5e90bbdb0bfb127dda0
kazukichi <zexyl_x@icloud.com>
2019-05-29 17:17:27 +0900

    layout-test

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  # viewのlayoutやlinkが合ってるかのテスト
+  test "layout links" do
+    get root_path
+    assert_template 'home_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "title"
+  end
+end
