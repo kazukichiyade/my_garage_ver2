eb716d2e590d62c899c51f1e9b68a0851a3cc153
kazukichi <zexyl_x@icloud.com>
2019-05-29 16:43:23 +0900

    layouts footer ruotes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'home_pages/home'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home_pages#home'
+  get 'signup', to: 'users#new'
 end
