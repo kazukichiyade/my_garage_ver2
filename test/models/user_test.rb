8e415b02b46cefe32de31ec8d5f3688e623b5c1c
kazukichi <zexyl_x@icloud.com>
2019-05-29 17:49:09 +0900

    Presence

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(name: "Example User", email: "user@examle.com")
+  end
+
+  # セットアップのこのユーザーは有効か？
+  test "should be valid?" do
+    assert @user.valid?
+  end
+
+  # name属性に存在性はあるか？
+  test "name should be presence" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+  # emailの存在性はあるか？
+  test "email should be presence" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
 end
