a35f45e37b99009aff9d076aa9cd82b814676838
kazukichi <zexyl_x@icloud.com>
2019-05-31 13:39:47 +0900

    Strong parameters shared/error-messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,23 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+
+    else
+      render 'new'
+    end
+  end
+
+  # 外部から使えない
+  private
+
+    # Strong Parameters(必須のパラメータと許可されたパラメータを指定)
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                      :password_confirmation)
+    end
 end
+
