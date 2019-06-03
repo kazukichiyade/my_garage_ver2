cc1ac2af1c176987fdaacd7cd19ea36154f24636
kazukichi <zexyl_x@icloud.com>
2019-06-03 19:30:29 +0900

    sessions_controller create and flash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    # ユーザーをデータベースから見つけて検証する
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+
+    else
+      flash.now[:danger] = "メールアドレスとパスワードのセットが違います"
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
