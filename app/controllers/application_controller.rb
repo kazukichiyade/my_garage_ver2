207d2af515b3e111e327d3515750c83a4e365b21
kazukichi <zexyl_x@icloud.com>
2019-06-04 14:32:42 +0900

    log_in(user) sessionメソッド

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionsHelper #controller内どこでも使えるようになる
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,9 @@ class SessionsController < ApplicationController
     # ユーザーをデータベースから見つけて検証する
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-
+      # 渡されたユーザーでログインする
+      log_in user #sessions_helper
+      redirect_to user  #user_url(user)
     else
       flash.now[:danger] = "メールアドレスとパスワードのセットが違います"
       render 'login'
