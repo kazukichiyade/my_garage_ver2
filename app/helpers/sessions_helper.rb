e43838d7ffefd1a53ecb58af02ec19e61d2f75f8
kazukichi <zexyl_x@icloud.com>
2019-06-04 15:00:32 +0900

    sessions_helper current_user logged_in?

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,4 +4,16 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  # 現在ログイン中のユーザーを返す (いる場合)
+  def current_user
+    if session[:user_id] #ユーザーIDが存在しない場合、コードが終了してnilを返す
+      @current_user ||= User.find_by(id: session[:user_id]) # User.find_byを使う事で最初の一回だけリクエスト
+    end
+  end
+
+  # ユーザーがログインしていればtrue、その他ならfalseを返す
+  def logged_in?
+    !current_user.nil?
+  end
 end
