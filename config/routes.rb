649f0368b9811d94313e78c3f5a33e1a926cbed1
kazukichi <zexyl_x@icloud.com>
2019-06-27 12:08:40 +0900

    password_resets generate view password_resets_new_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   root 'home_pages#home'
 
   get '/signup', to: 'users#signup' #new
@@ -10,4 +14,5 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activation, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
