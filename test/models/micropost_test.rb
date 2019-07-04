9c30e0d2d32d02cb476be64d69625ef6f0923e66
kazukichi <zexyl_x@icloud.com>
2019-07-04 21:56:52 +0900

    models micropost_test valid? presence

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:kazukichi)
+    # userに紐付いた新しいMicropostオブジェクトを返す
+    @micropost = @user.microposts.build(content: "aaa")
+  end
+
+  test "micropost should be valid" do
+    assert @micropost.valid?
+  end
+  
+  test "micropost should be presence" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
 end
